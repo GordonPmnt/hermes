5c2171fd9d274da2a4464b8f06f849a0cb79044d
Gordon Preumont <preumontgr@gmail.com>
2020-04-18 01:40:52 +0200

    get url of solutions

--- background.js
+++
@@ -1,6 +0,0 @@
-fetch('https://odyssey.wildcodeschool.com/users/17650?tab=quests')
-    .then(
-        response => response.text())
-    .then(
-        result => console.log(result)
-)
--- dashboard.js
+++ dashboard.js
@@ -4,15 +4,15 @@ window.addEventListener('load', () => {
         This block will be executed only if user is on the following URL: 
         https://odyssey.wildcodeschool.com/
     */
-   let urls = [
+    const urls = [
        "https://odyssey.wildcodeschool.com/", 
        "odyssey.wildcodeschool.com/", 
        "odyssey.wildcodeschool.com"
     ];
 
-   if(urls.includes(this.window.document.URL)) {
+    if(urls.includes(this.window.document.URL)) {
         let element = document.createElement("button");
-        let content = document.createTextNode("Hermes");
+        const content = document.createTextNode("Hermes");
         element.appendChild(content);
         element.id = "hermes-1";
        
@@ -20,15 +20,23 @@ window.addEventListener('load', () => {
         carousel.appendChild(element);
 
         let quests = this.document.getElementsByClassName("quests-list")[0].childNodes;
+        let solutions = [];
+
         for(let i=0; i<quests.length; i++){
             let quest = quests[i]
             if(quest.nodeType === 1 && quest.className !== "quest__link started border") {
-                console.log(quest)
+                solutionUrl = fetch(
+                    `https://odyssey.wildcodeschool.com${quest.pathname}`
+                ).then(
+                    response => response.url
+                ).then(
+                    url => solutions.push(url)
+                )
             }
         }
 
         element.addEventListener("click", () => {
-            // do something
+            console.log(solutions)
         });
     };
 });
