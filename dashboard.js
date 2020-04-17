76c7b616269eb2caa59a9256b854924970c93858
Gordon Preumont <preumontgr@gmail.com>
2020-04-18 00:57:30 +0200

    retrive solutioned quests

--- dashboard.js
+++ dashboard.js
@@ -4,20 +4,28 @@ window.addEventListener('load', function () {
         This block will be executed only if user is on the following URL: 
         https://odyssey.wildcodeschool.com/
     */
-   var urls = [
+   let urls = [
        "https://odyssey.wildcodeschool.com/", 
        "odyssey.wildcodeschool.com/", 
        "odyssey.wildcodeschool.com"
-    ]
+    ];
 
    if(urls.includes(this.window.document.URL)) {
-        var element = document.createElement("button");
-        var content = document.createTextNode("Hermes");
+        let element = document.createElement("button");
+        let content = document.createTextNode("Hermes");
         element.appendChild(content);
         element.id = "hermes-1";
        
-        var carousel = document.getElementsByClassName("carousel-cell is-selected")[0]
-        carousel.appendChild(element)
+        let carousel = document.getElementsByClassName("carousel-cell is-selected")[0];
+        carousel.appendChild(element);
+
+        let quests = this.document.getElementsByClassName("quests-list")[0].childNodes;
+        for(let i=0; i<quests.length; i++){
+            let quest = quests[i]
+            if(quest.nodeType === 1 && quest.className !== "quest__link started border") {
+                console.log(quest)
+            }
+        }
 
         element.addEventListener("click", function() {
             // do something
