ac815be226402e5592e658a3392a56df17ebfbfd
Gordon Preumont <preumontgr@gmail.com>
2020-04-18 06:12:28 +0200

    display popup

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+/*
+    Listen messages coming from content scripts and send a response
+*/
 chrome.runtime.onMessage.addListener(
     ({ id, notification }, sender, sendResponse) => {
         console.log(sender.tab 
@@ -9,3 +12,18 @@ chrome.runtime.onMessage.addListener(
         }
     }
 );
+
+/*
+    Allows extension to popup on odyssey.wildcodeschool.com domain
+*/
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+      chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [ new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: 'odyssey.wildcodeschool.com' },
+        })
+        ],
+            actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }]);
+    });
+  });
--- challenge.js
+++ challenge.js
@@ -53,5 +53,4 @@ window.addEventListener('load', () => {
             }, false);
         };
     }
-
 })
