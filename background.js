deefa657d656e4abe76fece6e56b7097089b8425
Gordon Preumont <preumontgr@gmail.com>
2020-04-20 20:57:00 +0200

    add google analytics

--- background.js
+++ background.js
@@ -26,4 +26,18 @@ chrome.runtime.onInstalled.addListener(() => {
             actions: [ new chrome.declarativeContent.ShowPageAction() ]
       }]);
     });
-  });
+});
+
+// Google Analytics
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-160726302-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript';
+  ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
